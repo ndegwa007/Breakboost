06fb4b92829481f1b3e0be3d1aa28bb5df20e372
ndegz <ndegwa8781@gmail.com>
2023-06-21 17:40:17 +0300

    initial commit with simple UI

---
+++ pomo-timer/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <main>
+      <Timer />
+    </main>
+  );
+}
+
+export default App;
---
+++ pomo-timer/src/Timer.js
@@ -0,0 +1,36 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
+
+
+const red = '#f54e4e';
+const green = '#4aec8c';
+
+
+function Timer() {
+    return (
+        <div>
+            <CircularProgressbar value={60} text={'60%'} styles={buildStyles({
+                textColor: '#fff',
+                pathColor: red,
+                trailColor: 'rgba(255,255,255,.2)'
+            })} />
+
+
+            <div style={{marginTop: '20px'}}>
+                <PlayButton />
+                <PauseButton />
+            </div>
+            
+            <div style={{marginTop: '20px'}}>
+                <SettingsButton />
+            </div>
+        </div>
+
+    );
+
+}
+
+export default Timer;
