dc381de822fd00211ce9f9003e67f9180a7b376c
ndegz <ndegwa8781@gmail.com>
2023-06-28 12:55:38 +0300

    pomotimer

--- pomo-timer/src/App.js
+++ pomo-timer/src/App.js
@@ -1,11 +1,31 @@
 //import logo from './logo.svg';
 import './App.css';
+import Settings from './Settings';
 import Timer from './Timer';
+import { useState } from 'react';
+import SettingsContext from './SettingsContext';
+
 
 function App() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
+   
     <main>
-      <Timer />
+       <SettingsContext.Provider value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setBreakMinutes,
+          setWorkMinutes 
+        }}>
+          
+          {showSettings ? <Settings /> : <Timer /> }
+        </SettingsContext.Provider>
+       
     </main>
   );
 }
--- pomo-timer/src/PlayButton.js
+++ pomo-timer/src/PlayButton.js
@@ -2,10 +2,10 @@ function PlayButton(props) {
     return(
         <button {...props}>
           
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
             </svg>
+
 
         </button>
     );
